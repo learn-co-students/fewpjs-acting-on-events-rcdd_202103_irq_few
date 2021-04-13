57e85fa466882c41e6e6b52885284bf354730ddf
Hasar Ali <hasar.ali@protonmail.com>
2021-04-13 13:43:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Your code here
+function move(node, refpt, amount, unit) {
+  let val = node.style[refpt].replace(unit, '');
+  val = parseInt(val, 10);
+  
+  const nodeWidth = node.offsetWidth;
+  const parentWidth = node.parentNode.offsetWidth
+  
+  
+  if (val - amount >= 0 && val - amount <= parentWidth - nodeWidth)
+    node.style[refpt] = `${val - amount}${unit}`;
+}
+
+function moveDodgerLeft() {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+ 
+  if (left > 0) 
+    dodger.style.left = `${left - 1}px`;
+}
+
+function moveDodgerRight() {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+ 
+  if (left < 500 - 40) 
+    dodger.style.left = `${left + 1}px`;
+}
+
+
+const dodger = document.querySelector('#dodger');
+document.addEventListener("keydown", e => {
+  if (e.key === 'ArrowLeft')
+    moveDodgerLeft();
+  if (e.key === 'ArrowRight')
+    moveDodgerRight();
+});
